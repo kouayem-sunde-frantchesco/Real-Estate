a10530ecc619159907bd1975eddefa394d3979ad
kouayem-sunde-frantchesco <kouayemfrantchesco@gmail.com>
2025-07-11 23:16:42 +0100

    initial commit

--- src/components/pages/login.js
+++ src/components/pages/login.js
@@ -21,7 +21,7 @@ const Login = () => {
     const password = e.target.password.value;
 
     try {
-      const response = await fetch(`${process.env.REACT_APP_API_URL}auth/login`, {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/auth/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }), // ✅ password correctement nommé
